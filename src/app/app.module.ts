23da0eff41ebec13f46968f426518c2aeaa6a797
Rodrigo Zagaceta <u202215489@upc.edu.pe>
2024-04-27 21:28:23 -0500

    chore: added home component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { HomeComponentComponent } from './public/pages/home/home.component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponentComponent
   ],
   imports: [
     BrowserModule,
