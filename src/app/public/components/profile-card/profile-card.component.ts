2ea5b11cacc42a80d3f4e3cc78f257da8b2696db
Rodrigo Zagaceta <u202215489@upc.edu.pe>
2024-04-28 18:06:24 -0500

    chore: update project.

--- src/app/public/components/profile-card/profile-card.component.ts
+++ src/app/public/components/profile-card/profile-card.component.ts
@@ -2,13 +2,17 @@ import { Component } from '@angular/core';
 import {MatCardModule} from "@angular/material/card";
 import {MatButtonModule} from "@angular/material/button";
 import {MatDivider} from "@angular/material/divider";
+import {FormsModule} from "@angular/forms";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {MatIconModule} from "@angular/material/icon";
 
 @Component({
   selector: 'app-profile-card',
   templateUrl: './profile-card.component.html',
   styleUrl: './profile-card.component.css',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule, MatDivider],
+  imports: [FormsModule, MatFormFieldModule, MatInputModule, MatIconModule, MatCardModule, MatButtonModule, MatDivider],
 })
 export class ProfileCardComponent {
 
