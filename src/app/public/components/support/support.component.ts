90a01ba3adbf8156043f4006386e869ef1933ca0
Jean Franco <u202219284@upc.edu.com>
2024-04-29 09:55:16 -0500

    feat: added close button for support component.

--- src/app/public/components/support/support.component.ts
+++ src/app/public/components/support/support.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import {MatCardModule} from "@angular/material/card";
 import {MatButtonModule} from "@angular/material/button";
+import {MatIcon} from "@angular/material/icon";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-support',
   templateUrl: './support.component.html',
   styleUrl: './support.component.css',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule],
+  imports: [MatCardModule, MatButtonModule, MatIcon, NgIf],
 })
 export class SupportComponent {
-
+  isVisible = true;
+  closeSupport() {
+    this.isVisible = false;
+  }
 }
